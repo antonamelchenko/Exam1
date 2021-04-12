7bda78e75668c6a0a656d6c83110cbf6ee885be1
antonamelchenko@inbox.ru <antonamelchenko@inbox.ru>
2021-04-12 15:17:47 +0300

    Добавьте файлы проекта.

---
+++ Exam/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace Exam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buStart = new System.Windows.Forms.Button();
+            this.laTimer = new System.Windows.Forms.Label();
+            this.laCorrect = new System.Windows.Forms.Label();
+            this.laIncorrect = new System.Windows.Forms.Label();
+            this.laLevel = new System.Windows.Forms.Label();
+            this.buIncrease = new System.Windows.Forms.Button();
+            this.buDecrease = new System.Windows.Forms.Button();
+            this.px = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.px)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buStart
+            // 
+            this.buStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buStart.Location = new System.Drawing.Point(463, 36);
+            this.buStart.Name = "buStart";
+            this.buStart.Size = new System.Drawing.Size(162, 69);
+            this.buStart.TabIndex = 0;
+            this.buStart.Text = "Старт";
+            this.buStart.UseVisualStyleBackColor = true;
+            // 
+            // laTimer
+            // 
+            this.laTimer.AutoSize = true;
+            this.laTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.laTimer.Location = new System.Drawing.Point(767, 62);
+            this.laTimer.Name = "laTimer";
+            this.laTimer.Size = new System.Drawing.Size(74, 25);
+            this.laTimer.TabIndex = 1;
+            this.laTimer.Text = "Время";
+            // 
+            // laCorrect
+            // 
+            this.laCorrect.AutoSize = true;
+            this.laCorrect.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.laCorrect.Location = new System.Drawing.Point(273, 36);
+            this.laCorrect.Name = "laCorrect";
+            this.laCorrect.Size = new System.Drawing.Size(68, 25);
+            this.laCorrect.TabIndex = 2;
+            this.laCorrect.Text = "Верно";
+            // 
+            // laIncorrect
+            // 
+            this.laIncorrect.AutoSize = true;
+            this.laIncorrect.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.laIncorrect.Location = new System.Drawing.Point(273, 74);
+            this.laIncorrect.Name = "laIncorrect";
+            this.laIncorrect.Size = new System.Drawing.Size(90, 25);
+            this.laIncorrect.TabIndex = 3;
+            this.laIncorrect.Text = "Неверно";
+            // 
+            // laLevel
+            // 
+            this.laLevel.AutoSize = true;
+            this.laLevel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.laLevel.Location = new System.Drawing.Point(72, 21);
+            this.laLevel.Name = "laLevel";
+            this.laLevel.Size = new System.Drawing.Size(88, 25);
+            this.laLevel.TabIndex = 4;
+            this.laLevel.Text = "Уровень";
+            // 
+            // buIncrease
+            // 
+            this.buIncrease.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buIncrease.Location = new System.Drawing.Point(52, 62);
+            this.buIncrease.Name = "buIncrease";
+            this.buIncrease.Size = new System.Drawing.Size(45, 47);
+            this.buIncrease.TabIndex = 5;
+            this.buIncrease.Text = "+";
+            this.buIncrease.UseVisualStyleBackColor = true;
+            // 
+            // buDecrease
+            // 
+            this.buDecrease.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buDecrease.Location = new System.Drawing.Point(115, 63);
+            this.buDecrease.Name = "buDecrease";
+            this.buDecrease.Size = new System.Drawing.Size(53, 46);
+            this.buDecrease.TabIndex = 6;
+            this.buDecrease.Text = "-";
+            this.buDecrease.UseVisualStyleBackColor = true;
+            // 
+            // px
+            // 
+            this.px.Location = new System.Drawing.Point(26, 141);
+            this.px.Name = "px";
+            this.px.Size = new System.Drawing.Size(823, 422);
+            this.px.TabIndex = 7;
+            this.px.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(886, 587);
+            this.Controls.Add(this.px);
+            this.Controls.Add(this.buDecrease);
+            this.Controls.Add(this.buIncrease);
+            this.Controls.Add(this.laLevel);
+            this.Controls.Add(this.laIncorrect);
+            this.Controls.Add(this.laCorrect);
+            this.Controls.Add(this.laTimer);
+            this.Controls.Add(this.buStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.px)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buStart;
+        private System.Windows.Forms.Label laTimer;
+        private System.Windows.Forms.Label laCorrect;
+        private System.Windows.Forms.Label laIncorrect;
+        private System.Windows.Forms.Label laLevel;
+        private System.Windows.Forms.Button buIncrease;
+        private System.Windows.Forms.Button buDecrease;
+        private System.Windows.Forms.PictureBox px;
+    }
+}
+
---
+++ Exam/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exam
+{
+    public partial class Form1 : Form
+    {
+        private const int SEC_MAX = 30;
+        private Button[,] bu;
+        private int chosen;
+        private readonly Timer tmDown = new Timer();
+        private int level;
+        private DateTime endTimeDown;
+        private int seconds;
+        private int correct = 0;
+        private int wrong = 0;
+        private Image[] img;
+
+        public int Rows { get; private set; } = 4;
+        public int Cols { get; private set; } = 4;
+        public Form1()
+        {
+            InitializeComponent();
+            tmDown.Interval = 100;
+            tmDown.Tick += TmDown_Tick;
+            endTimeDown = DateTime.Now.AddSeconds(SEC_MAX);
+            tmDown.Start();
+            level = 1;
+            CreateButtons();
+            ResizeButtons();
+
+
+            buIncrease.Click += BuIncrease_Click;
+            buDecrease.Click += BuDecrease_Click;
+            buStart.Click += BuStart_Click;
+        }
+
+        private void TmDown_Tick(object sender, EventArgs e)
+        {
+            var x = endTimeDown - DateTime.Now;
+            if (x.Ticks < 0)
+            {
+                tmDown.Stop();
+                x = TimeSpan.Zero;
+                MessageBox.Show($"Игра окончена. Верных ответов: {correct}");
+                BuStart_Click(sender, e);
+            }
+            seconds = x.Seconds;
+            laTimer.Text = $"Время: {seconds.ToString().PadLeft(2, '0')}";
+        }
+
+        private void BuDecrease_Click(object sender, EventArgs e)
+        {
+            if (level > 1)
+            {
+                --level;
+                laLevel.Text = $"Уровень {level}";
+                Rows--;
+                Cols--;
+                CreateButtons();
+                ResizeButtons();
+            }
+        }
+
+        private void BuIncrease_Click(object sender, EventArgs e)
+        {
+            if (level < 4)
+            {
+                ++level;
+                laLevel.Text = $"Уровень {level}";
+                Rows++;
+                Cols++;
+                CreateButtons();
+                ResizeButtons();
+            }
+        }
+
+        private void BuStart_Click(object sender, EventArgs e)
+        {
+            endTimeDown = DateTime.Now.AddSeconds(SEC_MAX);
+            tmDown.Start();
+            CreateButtons();
+            ResizeButtons();
+            wrong = 0;
+            correct = 0;
+            laIncorrect.Text = $"Неверно: {wrong}";
+            laCorrect.Text = $"Верно: {correct}";
+        }
+
+        private void ResizeButtons()
+        {
+            int xCellWidth = px.ClientSize.Width / Cols;
+            int xCellHeight = px.ClientSize.Height / Rows;
+
+            for (int i = 0; i < Rows; i++)
+                for (int j = 0; j < Cols; j++)
+                {
+                    bu[i, j].Width = xCellWidth;
+                    bu[i, j].Height = xCellHeight;
+                    bu[i, j].Location = new Point(xCellWidth * j, xCellHeight * i);
+                }
+        }
+
+        private void CreateButtons()
+        {
+            px.Controls.Clear();
+            bu = new Button[Rows, Cols];
+            
+            bool draw = true;
+            img = new Image[] {Properties.Resources.sqr_red,
+            Properties.Resources.sqr_yellow, Properties.Resources.blue
+            };
+            Random rnd = new Random();
+
+            
+            chosen = rnd.Next(img.Length);
+
+            for (int i = 0; i < Rows; i++)
+                for (int j = 0; j < Cols; j++)
+                {
+                    bu[i, j] = new Button();
+                    bu[i, j].Tag = null;
+                }
+
+            for (int i = 0; i < Rows; i++)
+                for (int j = 0; j < Cols; j++)
+                {
+                    int index = rnd.Next(img.Length);
+
+                    if (index == chosen)
+                    {
+                        if (draw)
+                        {
+                            bu[i, j].Image = img[index];
+                            bu[i, j].Tag = index;
+                            draw = false;
+                        }
+                        else
+                        {
+                            while (index == chosen)
+                            {
+                            index = rnd.Next(img.Length);
+                            }
+                            bu[i, j].Image = img[index];
+                            bu[i, j].Tag = index;
+                        }
+                    }
+                    else
+                    {
+                        bu[i, j].Image = img[index];
+                    }
+
+                    bu[i, j].Click += Form1_Click; 
+                    px.Controls.Add(bu[i, j]);
+                }
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            if (Convert.ToInt32(btn.Tag) == chosen)
+            {
+                correct++;
+                laCorrect.Text = $"Верно: {correct}";
+            }
+            else
+            {
+                wrong++;
+                laIncorrect.Text = $"Неверно: {wrong}";
+            }
+            CreateButtons();
+            ResizeButtons();
+        }
+
+    }
+}
